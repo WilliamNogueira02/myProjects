1eaa8fa4e0a5456e048c835069cfc598cf8b7e81
William Nogueira <willalmeida1215@gmail.com>
2025-11-02 21:20:11 -0300

    To do List
    A little project of To do List.

---
+++ To-do-list/script.js
@@ -0,0 +1,22 @@
+let BotaoAdd = document.getElementById('botao');
+let Input = document.getElementById('input-tarefa');
+let Tarefas = document.getElementById('tarefas');
+
+BotaoAdd.addEventListener('click', function() {
+    if (Input.value != '') {
+        var tarefa = document.createElement('p');
+        Tarefas.appendChild(tarefa);
+        tarefa.innerText = '- ' + Input.value;
+        Input.value = '';
+        tarefa.addEventListener('click', function() {
+            if (tarefa.style.textDecoration === 'line-through') {
+                tarefa.style.textDecoration = 'none';
+            } else {
+                tarefa.style.textDecoration = 'line-through';
+            }
+        });
+        tarefa.addEventListener('dblclick', function() {
+            Tarefas.removeChild(tarefa);
+        });
+    }
+});
