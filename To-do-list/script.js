5bf5d995b75ec98478e4a53a8376ea8bb4c7bdfb
William Nogueira <140857832+WilliamNogueira02@users.noreply.github.com>
2026-01-07 00:03:30 -0300

    Add files via upload

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -1,22 +1,29 @@
-let BotaoAdd = document.getElementById('botao');
-let Input = document.getElementById('input-tarefa');
-let Tarefas = document.getElementById('tarefas');
+let BotaoAdd = document.getElementById("botao");
+let BotaoRemove = document.getElementById("botao-remover");
+let Input = document.getElementById("input-tarefa");
+let Tarefas = document.getElementById("tarefas");
 
-BotaoAdd.addEventListener('click', function() {
-    if (Input.value != '') {
-        var tarefa = document.createElement('p');
-        Tarefas.appendChild(tarefa);
-        tarefa.innerText = '- ' + Input.value;
-        Input.value = '';
-        tarefa.addEventListener('click', function() {
-            if (tarefa.style.textDecoration === 'line-through') {
-                tarefa.style.textDecoration = 'none';
-            } else {
-                tarefa.style.textDecoration = 'line-through';
-            }
-        });
-        tarefa.addEventListener('dblclick', function() {
-            Tarefas.removeChild(tarefa);
-        });
-    }
+BotaoAdd.addEventListener("click", function () {
+  if (Input.value != "") {
+    var tarefa = document.createElement("p");
+    Tarefas.appendChild(tarefa);
+    tarefa.innerText = "- " + Input.value;
+    Input.value = "";
+    tarefa.addEventListener("click", function () {
+      if (tarefa.style.textDecoration === "line-through") {
+        tarefa.style.textDecoration = "none";
+      } else {
+        tarefa.style.textDecoration = "line-through";
+      }
+    });
+    tarefa.addEventListener("dblclick", function () {
+      Tarefas.removeChild(tarefa);
+    });
+  }
+});
+
+BotaoRemove.addEventListener("click", function () {
+  if (Tarefas.lastChild) {
+    Tarefas.removeChild(Tarefas.lastChild);
+  }
 });
